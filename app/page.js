9fe663184e49ae2a5fa22aedd97db851b423723c
Mubashir <mubashirseo100@gmail.com>
2024-12-07 20:59:00 +0500

    first commit

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 import Image from "next/image";
-
+import Header from "@/components/header/header";
 export default function Home() {
   return (
    <div>
+    <Header/>
     </div>
   );
 }
---
+++ components/header/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className='flex'>
+        <div className='bg-slate-700'>left sideasdssadad</div>
+        <div className='bg-green-700'>right sideasdsadads</div>
+    </div>
+  )
+}
+
+export default Header
